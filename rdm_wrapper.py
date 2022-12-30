e7a15fe9515210b5dceed4325edcd07bc9b186c5
Patrick Kane <pkane@thewaterinstitute.org>
2022-12-30 15:20:51 -0600

    add my files

---
+++ rdm_wrapper.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+#filler code, takes an outlet parameter list specified as a .csv file and turns it into the appropriate
+#file type for running hec ras
+def preprocess_dss(outlet_file):
+    print("turning "+outlet_file+"into csv")
+
+#filler code for running hec ras once the appropriate dss files have been written
+def run_hec_ras():
+    print("running hec-ras on temp outlet file")
+
+#rdm_wrapper that takes in an outlet file, preprocesses it to get a specific configuration for a given
+#uncertainty state and then sets the values for specific outlets chosen to be levers
+#takes 2 inputs specific inputs:
+#   *outlet_uncertainty_path is the path to a .csv file containing a list of outlets and their parameterizations
+#   under different uncertainity scenarios (see file Outlet_treated_as_uncertainies.csv for example)
+#   *outlet_uncertainty_configuration selects which column in outlet_uncertainty_path to use for parameterizing
+#   this assumes that uncertainties will be more about configurations (leaky vs. non-leaky) instead of ranges of values
+#it also takes an arbitrary number of named lever inputs, which overwrite the parameters for the named outlet
+def rdm_wrapper(outlet_uncertainty_path, outlet_uncertainty_configuration, **levers):
+    #reads in the preprocessed data - i think eventually we will want to connect this to air table
+    outlet_uncertainty_data = pd.read_csv(outlet_uncertainty_path, index_col=0)[outlet_uncertainty_configuration]
+    
+    #loops through provided level arguments
+    for key, value in levers.items():
+        if(key in outlet_uncertainty_data):
+            #overwrites the parameter for the specified outlet
+            #currently only works for constant flow style outlet
+            #additional preprocessing will be needed for later weirs and rating curves
+            param_strings=outlet_uncertainty_data.at[key].split(", ")
+            param_strings_new=param_strings[0]+", "+str(value)
+            outlet_uncertainty_data.at[key]=param_strings_new
+            
+    #writes the processed data to a file
+    outlet_uncertainty_data.to_csv("temp.csv")
+    #reads the processed data file and turns it into the appropriate hec ras file type
+    #the handoff doesn't necessairly need to involve a temp file I just thought it was a helpful demo
+    preprocess_dss("temp.csv")
+    #run hec-ras using the preprocessed file
+    run_hec_ras()
+
+    
+
+x=rdm_wrapper("Outlet_treated_as_uncertainties.csv", "Uncertainty_1", Outlet_1=3, Outlet_7=5)
